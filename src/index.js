017c52bdef414afdede0664e063880e058bd2e65
vsrimal <vishwadeaero@gmail.com>
2023-09-12 21:30:24 +0530

    Initialize the project with essentials

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => res.send('Smart-Attendance-API'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
