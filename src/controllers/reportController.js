61b8551c4a6d22e9c9cb9176624cd1745ec82039
vsrimal <vishwadeaero@gmail.com>
2024-04-27 17:47:32 +0530

    Subject Attendance API added

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -46,6 +46,61 @@ const getStudentAttendance = async (req, res) => {
     }
 };
 
+const getSubjectAttendance = async (req, res) => {
+    try {
+        const { body } = req;
+        const subjectId = body.subjectId;
+        const startDate = (body.startDate) ? new Date(body.startDate) : new Date(0);
+        const endDate = (body.endDate) ? new Date(body.endDate) : new Date();
+
+        // Get student enrolled subjects
+        const enrolments = await enrolmentService.getEnrolledStudents(subjectId);
+
+        // Get lectures of subject
+        const subjectLectures = await lectureService.getAllLecturesBySubject(subjectId);
+
+        // Get attenedance of student
+        let counter = 1;
+        const attendances = await Promise.all(subjectLectures.map(async (lecture) => {
+            const studentAttendance = await Promise.all(enrolments.map(async (enrolment) => {
+                const attendance = await attendanceService.getAttendanceByStudentLecture(enrolment.studentId, lecture.id);
+                const studentData = {
+                    id: counter++,
+                    name: enrolment.student.name,
+                    indexNo: enrolment.student.indexNo,
+                    lecture: lecture,
+                    attendedAt: (attendance) ? attendance.attendedAt : null
+                }
+                return studentData;
+            }))
+            return studentAttendance; // Return the modified lecture object
+        }));
+
+        const subjectAttendance = attendances.flat();
+        // Sort the data by the 'id' field
+        subjectAttendance.sort((a, b) => a.id - b.id);
+
+        // Filter Subject Attendance by Date
+        const filteredAttendances = subjectAttendance.filter(attendance => {
+            const scheduledAt = new Date(attendance.lecture.scheduledAt);
+            return scheduledAt > startDate && scheduledAt <= endDate;
+        });
+
+
+        res.status(200).json({
+            status: 'OK',
+            data: filteredAttendances
+        });
+    } catch (error) {
+        // Handle errors and send an error response
+        console.log(error)
+        res.status(500).json({
+            error: 'Internal server error',
+            details: error
+        });
+    }
+};
+
 const getLectureAttendance = async (req, res) => {
     try {
         const { body } = req;
@@ -56,17 +111,19 @@ const getLectureAttendance = async (req, res) => {
         let enrolments = await enrolmentService.getEnrolledStudents(subjectId);
 
         // Get attenedance of lecture
+        let counter = 1;
         const attendances = await Promise.all(enrolments.map(async (enrolment) => {
             const attendance = await attendanceService.getAttendanceByStudentLecture(enrolment.studentId, lectureId);
             const studentData = {
-                id: enrolment.student.id,
+                id: counter++,
                 name: enrolment.student.name,
                 indexNo: enrolment.student.indexNo,
                 attendedAt: (attendance) ? attendance.attendedAt : null
             }
             return studentData; // Return the modified lecture object
         }));
-
+        // Sort the data by the 'id' field
+        attendances.sort((a, b) => a.id - b.id);
 
         res.status(200).json({
             status: 'OK',
@@ -84,5 +141,6 @@ const getLectureAttendance = async (req, res) => {
 
 module.exports = {
     getStudentAttendance,
+    getSubjectAttendance,
     getLectureAttendance
 }
--- src/routes/v1/reportRouter.js
+++ src/routes/v1/reportRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { protected } = require('../../middleware/auth');
 const router = express.Router();
-const { getStudentAttendance, getLectureAttendance } = require('../../controllers/reportController');
+const { getStudentAttendance, getLectureAttendance, getSubjectAttendance } = require('../../controllers/reportController');
 
 router.post("/student", protected(['Lecturer','Administrator']), getStudentAttendance);
 router.post("/lecture", protected(['Lecturer','Administrator']), getLectureAttendance);
+router.post("/subject", protected(['Lecturer','Administrator']), getSubjectAttendance);
 
 module.exports = router;
