d6f66afb616c10d7db99f5834e538dd025e092d7
vsrimal <vishwadeaero@gmail.com>
2024-05-03 21:23:53 +0530

    upcoming lectures (app) API added

--- src/routes/v1/lectureRouter.js
+++ src/routes/v1/lectureRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { protected } = require('../../middleware/auth');
+const { protected, studentOnly } = require('../../middleware/auth');
 const router = express.Router();
 
 const {
@@ -9,10 +9,12 @@ const {
     generateLectureQR,
     createLecture,
     updateLecture,
-    deleteLecture 
+    deleteLecture, 
+    getAllUpcomingLectures
 } = require('../../controllers/lectureController');
 
 router.get("/", getAllLectures);
+router.get("/upcoming", studentOnly, getAllUpcomingLectures);
 router.get("/:lectureId", getOneLecture);
 router.post("/", protected(['lecturer']), createLecture);
 router.patch("/:lectureId", protected(['lecturer']), updateLecture);
